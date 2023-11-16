b40154f594cc7f808833927255f322db5b39d15b
nickh <nickhadzi123@gmail.com>
2023-11-16 14:15:52 -0500

    All fixes for the login and registration UI made, still need to fix registration page and connect DB

--- src/main/java/com/capstoneproject/ForeverHomeFinderMain.java
+++ src/main/java/com/capstoneproject/ForeverHomeFinderMain.java
@@ -2,22 +2,67 @@ package com.capstoneproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class ForeverHomeFinderMain extends Application {
 
+    private Stage primaryStage;
+
     @Override
-    public void start(Stage stage) throws IOException {//Sets up GUI for window
-        FXMLLoader fxmlLoader = new FXMLLoader(ForeverHomeFinderMain.class.getResource("ForeverHomeFinderGUI-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Forever Home Finder");//Sets title for window
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        showLoginScene();
     }
 
-    public static void main(String[] args) {//Main Method
-        launch();
+    private void showLoginScene() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+            Parent root = loader.load();
+            primaryStage.setResizable(false);
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+            LoginController loginController = loader.getController();
+            loginController.setMainApplication(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showRegistrationScene() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("registration.fxml"));
+            Parent root = loader.load();
+            primaryStage.setTitle("User Registration");
+            primaryStage.setScene(new Scene(root));
+
+            RegistrationController registrationController = loader.getController();
+            registrationController.setStage(primaryStage);
+            registrationController.setMainApplication(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showMainApplication() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
+            Parent root = loader.load();
+            primaryStage.setTitle("Animal Dashboard");
+            primaryStage.setScene(new Scene(root));
+
+            DashboardController dashboardController = loader.getController();
+            dashboardController.initialize(); // Initialize animal list
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ src/main/java/com/capstoneproject/LoginController.java
@@ -0,0 +1,65 @@
+package com.capstoneproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private ImageView logoImageView;
+    private ForeverHomeFinderMain mainApp;
+
+    public void initialize() {
+        // This method will be automatically called when the FXML is loaded
+        Image image = new Image(getClass().getResourceAsStream("/FHFlogo.png"));
+        logoImageView.setImage(image);
+    }
+
+    public void setMainApplication(ForeverHomeFinderMain mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    public void loginButtonClicked(ActionEvent actionEvent) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // Perform actual login authentication logic here
+        boolean authenticationSuccessful = authenticateUser(username, password);
+
+        if (authenticationSuccessful) {
+            // If login successful, proceed to the main application or user dashboard
+            mainApp.showMainApplication(); // Method to display the main application or dashboard
+        } else {
+            showAlert("Login Failed", "Invalid username or password. Please try again.");
+        }
+    }
+
+    private boolean authenticateUser(String username, String password) {
+        // Placeholder for authentication logic (replace with your actual authentication mechanism)
+        // Example: Check against a database or hardcoded credentials
+        return username.equals("exampleUser") && password.equals("examplePassword");
+    }
+
+    public void registerButtonClicked(ActionEvent actionEvent) {
+        mainApp.showRegistrationScene();
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.show();
+    }
+}
